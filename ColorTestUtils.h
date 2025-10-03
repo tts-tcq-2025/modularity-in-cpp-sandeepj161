ca7508f6b61da385d2e191b85d01292692478587
Sandeep J <j.sandeep.private@gmail.com>
2025-10-03 20:16:35 +0530

    Create ColorTestUtils.h

---
+++ ColorTestUtils.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <cassert>
+#include <string>
+#include "ColorCode.h"
+#include "ColorPair.h"
+#include "ManualFormatter.h"
+
+namespace TelCoColorCoder::TestUtils {
+
+inline void validateAllPairs() {
+    for (int n = 1; n <= totalPairs(); ++n) {
+        auto [maj, min] = colorFromPairNumber(n);
+        assert(pairNumberFromColor(maj, min) == n);
+    }
+}
+
+inline void validateSpecificPairs() {
+    auto [m4, s4] = colorFromPairNumber(4);
+    assert(m4 == MajorColor::White && s4 == MinorColor::Brown);
+
+    auto [m5, s5] = colorFromPairNumber(5);
+    assert(m5 == MajorColor::White && s5 == MinorColor::Slate);
+
+    assert(pairNumberFromColor(MajorColor::Black, MinorColor::Orange) == 12);
+    assert(pairNumberFromColor(MajorColor::Violet, MinorColor::Slate) == 25);
+}
+
+inline void validateInvalidInputs() {
+    try { colorFromPairNumber(0); assert(false); } catch (const std::out_of_range&) {}
+    try { colorFromPairNumber(26); assert(false); } catch (const std::out_of_range&) {}
+}
+
+inline void validateNamesAndStrings() {
+    assert(std::string(majorColorName(MajorColor::Yellow)) == "Yellow");
+    assert(std::string(minorColorName(MinorColor::Slate)) == "Slate");
+    assert((ColorPair{MajorColor::Red, MinorColor::Green}.toString() == std::string("Red Green")));
+}
+
+inline void validateReferenceManual() {
+    const std::string manual = makeReferenceManual();
+    assert((manual.find("1 - White Blue") != std::string::npos));
+    assert((manual.find("25 - Violet Slate") != std::string::npos));
+}
+
+} // namespace TestUtils
